69bd70f16748723b5c7ef71ca5ab06ac7611b49d
JessicaVitanza <jessicavitanzauidesign@gmail.com>
2023-01-11 18:10:40 +0100

    commit

---
+++ script.js
@@ -0,0 +1,50 @@
+window.addEventListener('scroll', () => {  
+    let scrollTop = document.documentElement.scrollTop;        
+    document.querySelector('.hero img').style.width = 100 + scrollTop / 10 + '%';
+});
+
+
+
+
+
+const scrollElements = document.querySelectorAll(".box-intro");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.35)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
+
